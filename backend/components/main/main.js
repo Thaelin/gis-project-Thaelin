4e404341bd9264d1defab461ece65daa2b0e5a57
Thaelin <desino.SK@gmail.com>
2019-12-12 11:28:14 +0100

    fix2

--- backend/components/main/main.js
+++ backend/components/main/main.js
@@ -42,6 +42,31 @@ class Main {
                 console.log(req.params);
                 res.sendFile(path.join(__dirname, '../../../frontend/index.html'));
             });
+            
+            // API route for getting all cycling routes
+            this.app.get('/api/cyclingRoutes', (req, res) => {
+                this.db.allCyclingRoutes((error, data) => {
+                    if (error) {
+                        this.logger.error(error);
+                        throw new Error;
+                    }
+                    else {
+                        let parsedData = [];
+
+                        // parse data to JSON
+                        data.rows.forEach((route) => {
+                            parsedData.push({
+                                fid: route.fid,
+                                name: route.name,
+                                route: JSON.parse(route.route),
+                                length: route.length
+                            });
+                        });
+
+                        res.json(parsedData);
+                    }
+                });
+            });
 
             // API route for getting specific route's weather points
             this.app.get('/api/weatherPoints/:routeId', (req, res) => {
@@ -108,7 +133,32 @@ class Main {
                 }
             });
 
-            this.app.get('/api/shortestPath/:lat/:lon/:region/:minTemp/:maxTemp', (req, res) => {
+            this.app.get('/api/test', (req, res) => {
+                this.db.testProjection((error, data) => {
+                    if (error) {
+                        this.logger.error(error);
+                        throw new Error;
+                    }
+                    else {
+                        res.json(data.rows);
+                    }
+                });
+                
+            });
+
+            this.app.get('/api/allRoads', (req, res) => {
+                this.db.getLoadedRoads((error, data) => {
+                    if (error) {
+                        this.logger.error(error);
+                        throw new Error;
+                    }
+                    else {
+                        res.json(data.rows);
+                    }
+                });
+            });
+
+            this.app.get('/api/shortestPath/:lat/:lon/:mapPart/:minTemp/:maxTemp', (req, res) => {
                 if (isNaN(req.params.lat) || isNaN(req.params.lon)) {
                     this.logger.warn(`Received /api/shortestPath/:lat/:lon with invalid parameters`);
                     res.status(400).json({
@@ -116,7 +166,7 @@ class Main {
                         errorMsg: `Received /api/shortestPath/:lat/:lon with invalid parameters - is not a number`
                     });
                 }
-                this.db.getShortestPath(req.params.lat, req.params.lon, req.params.region, req.params.minTemp, req.params.maxTemp, (error, data) => {
+                this.db.getShortestPath(req.params.lat, req.params.lon, req.params.mapPart, req.params.minTemp, req.params.maxTemp, (error, data) => {
                     if (error) {
                         console.log(error);
                         this.logger.error(error);
@@ -140,10 +190,10 @@ class Main {
                 });
             });
 
-            this.app.get('/api/cyclingRoutesFilter/:region/:minTemp/:maxTemp', (req, res) => {
+            this.app.get('/api/cyclingRoutesFilter/:route/:minTemp/:maxTemp', (req, res) => {
                 if (req.params.route && req.params.minTemp && req.params.maxTemp) {
                     this.db.cyclingRoutesFiltered(
-                        req.params.region, req.params.minTemp, req.params.maxTemp, 
+                        req.params.route, req.params.minTemp, req.params.maxTemp, 
                         (error, data) => {
                             if (error) {
                                 console.log(error);
@@ -170,10 +220,10 @@ class Main {
                     );
                 }
                 else {
-                    this.logger.warn(`Received /api/cyclingRoutesFilter/:region/:minTemp/:maxTemp with invalid parameters`);
+                    this.logger.warn(`Received /api/cyclingRoutesFilter/:route/:minTemp/:maxTemp with invalid parameters`);
                     res.status(400).json({
                         errorCode: 'PARAMETER_EMPTY',
-                        errorMsg: `Received /api/cyclingRoutesFilter/:region/:minTemp/:maxTemp with invalid parameters - is empty`
+                        errorMsg: `Received /api/cyclingRoutesFilter/:route/:minTemp/:maxTemp with invalid parameters - is empty`
                     });
                 }
             });
